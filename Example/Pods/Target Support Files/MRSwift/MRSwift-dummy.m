582dbe93a80f6cffb280180a9c2eb2e447e03187
Nicola Innocenti <nicolainnocenti@MacBook-Pro-di-Nicola-MrApps.local>
2017-06-13 11:22:49 +0200

    Removed pods

--- Example/Pods/Target Support Files/MRSwift/MRSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MRSwift : NSObject
-@end
-@implementation PodsDummy_MRSwift
-@end
--- Example/Pods/Target Support Files/Pods-MRSwiftExample/Pods-MRSwiftExample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MRSwiftExample : NSObject
-@end
-@implementation PodsDummy_Pods_MRSwiftExample
-@end
