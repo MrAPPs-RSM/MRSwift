87c9feabce3dd3f31d5272b21f4796db227bdd32
Nicola Innocenti <nicolainnocenti@MacBook-Pro-di-Nicola-MrApps.local>
2018-02-07 15:44:17 +0100

    Minor

--- MRSwift/Classes/Utils/MRExtensions.swift
+++ MRSwift/Classes/Utils/MRExtensions.swift
@@ -52,7 +52,7 @@ public extension URL {
 
 public extension String {
     
-    public func localized(name: String) -> String {
+    public class func localized(name: String) -> String {
         return NSLocalizedString(name, comment: "")
     }
     
