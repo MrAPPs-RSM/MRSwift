a97cffe3710e0915bc0cf382c0af2954255c175e
Nicola Innocenti <nicolainnocenti@MacBook-Pro-di-Nicola-MrApps.local>
2018-03-06 17:38:20 +0100

    Rework MRCreditsViewController

--- MRSwift/Classes/Controllers/MRCreditsViewController.swift
+++ MRSwift/Classes/Controllers/MRCreditsViewController.swift
@@ -12,7 +12,7 @@ public class CreditsElement {
     var title = ""
     var image = ""
     var link = ""
-
+    
     init(title : String, image : String, link : String) {
         self.title = title
         self.image = image
@@ -22,17 +22,20 @@ public class CreditsElement {
 
 open class MRCreditsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    // MARK: - Xibs
+    
     @IBOutlet private var lblAppName : UILabel!
     @IBOutlet private var lblIntroMrApps : UILabel!
     @IBOutlet private var imgHeaderBackground : UIImageView!
-    @IBOutlet private var gradientView : UIView!
-    @IBOutlet private var backButton : UIButton!
     @IBOutlet private var headerView : UIView!
-    @IBOutlet private var lblNavTitle : UILabel!
     @IBOutlet private var tableView : UITableView!
+    
+    // MARK: - Constraints
+    
     @IBOutlet private var cntTableViewTrailing : NSLayoutConstraint!
     @IBOutlet private var cntTableViewLeading : NSLayoutConstraint!
-    @IBOutlet weak var cntGradientViewHeight: NSLayoutConstraint!
+    
+    // MARK: - Constants & Variables
     
     private var elements : [CreditsElement] = []
     private var bundle : Bundle!
@@ -40,11 +43,13 @@ open class MRCreditsViewController: UIViewController, UITableViewDelegate, UITab
     private var isPresenting = false
     private var cellIdentifier = "cellIdentifier"
     private var localizationBundle : Bundle!
-
-    var appName : String = ""
-    var color : UIColor!
-    var regularFont : UIFont!
-    var boldFont : UIFont!
+    
+    public var appName : String = ""
+    public var color : UIColor!
+    public var regularFont : UIFont!
+    public  var boldFont : UIFont!
+    
+    // MARK: - Initialization
     
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -52,12 +57,10 @@ open class MRCreditsViewController: UIViewController, UITableViewDelegate, UITab
     
     public init(appName:String?, color:UIColor?, regularFont:UIFont?, boldFont:UIFont?) {
         
-
-        self.bundle = Bundle(for:MRCreditsViewController.self)
-        self.localizationBundle = Bundle(path: self.bundle.bundlePath + "/CreditsLocalizations.bundle")
-
-        super.init(nibName:"MRCreditsViewController", bundle:self.bundle)
+        bundle = Bundle(for:MRCreditsViewController.self)
+        localizationBundle = Bundle(path: self.bundle.bundlePath + "/CreditsLocalizations.bundle")
         
+        super.init(nibName:"MRCreditsViewController", bundle: bundle)
         
         if let appName = appName {
             self.appName = appName
@@ -82,7 +85,6 @@ open class MRCreditsViewController: UIViewController, UITableViewDelegate, UITab
         } else {
             self.boldFont = UIFont.boldSystemFont(ofSize: 15.0)
         }
-        
     }
     
     convenience public init() {
@@ -97,16 +99,21 @@ open class MRCreditsViewController: UIViewController, UITableViewDelegate, UITab
         self.init(appName: nil, color: color, regularFont: nil, boldFont: nil)
     }
     
+    // MARK: - UIViewController Methods
+    
     override open func viewDidLoad() {
         
         super.viewDidLoad()
         
+        navigationItem.title = "Credits"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "ico_back", in: bundle, compatibleWith: self.traitCollection)?.paint(with: UIColor.white), style: .plain, target: self, action: #selector(didTapBackButton))
+        
         lblAppName.text = self.appName
         
         tableView.tableHeaderView = self.headerView
-        tableView.alwaysBounceVertical = false
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
-        tableView.contentInset = .zero
+        tableView.alwaysBounceVertical = false
+        tableView.contentInset = UIEdgeInsets(top: -UIView.safeArea.top, left: 0, bottom: 0, right: 0)
         
         elements = [
             CreditsElement(title: NSLocalizedString("Scopri Mr. APPs", tableName: nil, bundle: localizationBundle, value: "", comment: ""),
@@ -136,77 +143,9 @@ open class MRCreditsViewController: UIViewController, UITableViewDelegate, UITab
         lblIntroMrApps.text = NSLocalizedString("è un'applicazione realizzata da Mr. APPs", tableName: nil, bundle: localizationBundle, value: "", comment: "")
         lblIntroMrApps.font = regularFont
         lblAppName.font = boldFont
-        
-        navGradient = CAGradientLayer()
-        navGradient.frame = gradientView.bounds
-        navGradient.colors = [
-            UIColor.black.withAlphaComponent(0.8).cgColor,
-            UIColor.black.withAlphaComponent(0.3).cgColor,
-            UIColor.clear.cgColor
-        ]
-        
-        gradientView.layer.insertSublayer(navGradient, at: 0)
-        cntGradientViewHeight.constant = 64.0 + UIView.safeArea.top
-        
-        
-        if let boldFont = boldFont {
-            lblNavTitle.font = boldFont
-        }
-        
-        if navigationController != nil && navigationController!.viewControllers.count > 1  {
-            backButton.setImage(UIImage(named: "ico_back", in: bundle, compatibleWith: self.traitCollection)?.paint(with: UIColor.white), for: .normal)
-            backButton.addTarget(self, action: #selector(didTapBackButton), for: .touchUpInside)
-        } else  {
-            backButton.setImage(UIImage(named: "ico_nav_chiudi", in: bundle, compatibleWith: self.traitCollection)?.paint(with: UIColor.white), for: .normal)
-            backButton.addTarget(self, action: #selector(dismissView), for: .touchUpInside)
-        }
-        
     }
     
-    open override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        if !isPresenting {
-            navigationController?.setNavigationBarHidden(true, animated: true)
-        }
-    }
-    
-    open override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        if !isPresenting {
-            navigationController?.setNavigationBarHidden(false, animated: true)
-        }
-    }
-    
-    override open func viewDidAppear(_ animated: Bool) {
-        isPresenting = false
-    }
-    
-    override open func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        navGradient.frame = gradientView.bounds
-    }
-    
-    func showNavigationBar(_ show : Bool) {
-        
-        if show {
-            navigationController?.navigationBar.setBackgroundImage(nil, for: .default)
-            navigationController?.navigationBar.shadowImage = nil;
-            navigationController?.navigationBar.isTranslucent = false;
-        } else {
-            navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
-            navigationController?.navigationBar.shadowImage = UIImage();
-            navigationController?.navigationBar.isTranslucent = true;
-        }
-        
-    }
-    
-    @objc open func dismissView() {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    //Tableview delegate methods
+    //UITableView DataSource & Delegate
     
     open func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -233,53 +172,9 @@ open class MRCreditsViewController: UIViewController, UITableViewDelegate, UITab
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    // MARK: - Other Methods
+    
     @objc open func didTapBackButton() {
         navigationController?.popViewController(animated: true)
     }
 }
-    
-    
-    
-    // MARK: - Initialization
-    /*
-    init(){
-        super.init(nibName: "MRCreditsViewController", bundle: MRCreditsViewController.self)
-    }
-    
-    convenience override init(appName: String, tintColor: UIColor, regularFont: UIFont, boldFont: UIFont) {
-        self.init()
-        
-        self.appName = appName
-        self.mainColor = tintColor
-        self.regularFont = regularFont
-        self.boldFont = boldFont
-        //self.iconsPath = "\(Bundle.main.pathForResource("MRCreditsViewController", ofType: "bundle")!)/"
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-    
-    // MARK: - UIViewController Functions
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        if !UIDevice.isIpad() {
-            self.backButton.setImage(UIImage(named: "ico_indietro"), forState: .Normal)
-            self.backButton.addTarget(self, action: #selector(CreditsViewController.didTapBackButton), forControlEvents: .TouchUpInside)
-        }
-    }
-    
-    // MARK: - UIBarButtonItems Handlers
-    
-    func didTapBackButton() {
-        self.navigationController?.popViewControllerAnimated(true)
-    }
-    
-    // MARK: - Others Functions
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }*/
