493e128f1a01a5437ca13f811ca0c066b16eb84a
Nicola Innocenti <nicolainnocenti@MacBook-Pro-di-Nicola-MrApps.local>
2018-04-09 15:05:24 +0200

    Added string validation for phone number

--- MRSwift/Classes/Utils/MRExtensions.swift
+++ MRSwift/Classes/Utils/MRExtensions.swift
@@ -66,10 +66,17 @@ public extension String {
     public var isValidEmail : Bool {
         do {
             let regex = try NSRegularExpression(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}", options: .caseInsensitive)
-            return regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.characters.count)) != nil
+            return regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.count)) != nil
         } catch {
             return false
         }
+    }
+    
+    public var isValidPhoneNumber : Bool {
+        
+        let containsNumbers = self.rangeOfCharacter(from: .decimalDigits) != nil
+        let isLongEnough = self.count > 5
+        return containsNumbers && isLongEnough
     }
     
     public var isValidRemoteUrl : Bool {
